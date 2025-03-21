dd076627c2c7c158b048bce4a0b93a5b85f5254c
rajan <rajan@gmail.com>
2025-03-21 11:32:51 +0530

    scrollbar hide

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { lazy } from "react";
+import React, { lazy } from "react";
 import "./App.css";
-
 const ThemeRoutes = lazy(() => import(`./ThemeRoutes`));
+
 function App() {
+  React.useEffect(() => {
+    const disableLoggingInProduction = () => {
+      if (process.env.NODE_ENV === 'production') {
+        console.log = () => { };
+        console.warn = () => { };
+        console.error = () => { };
+        console.info = () => { };
+        console.debug = () => { };
+      }
+    };
+
+    disableLoggingInProduction();
+    return () => {
+      if (process.env.NODE_ENV === 'production') {
+        console.log = console.__log;
+        console.warn = console.__warn;
+        console.error = console.__error;
+        console.info = console.__info;
+        console.debug = console.__debug;
+      }
+    };
+  }, []);
+  
   return (
     <>
       <RecoilRoot>
